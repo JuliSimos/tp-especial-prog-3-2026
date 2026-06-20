48affeb709551c3f01894ea6a2ca06bb7baebf9b
Julieta Simos <juliet.simos@gmail.com>
2026-06-19 21:31:03 -0300

    Ultima limpieza del codigo

--- src/distribucion/backtracking/DistribucionBacktracking.java
+++ src/distribucion/backtracking/DistribucionBacktracking.java
@@ -125,14 +125,6 @@ public class DistribucionBacktracking {
         }
         return suma;
     }
-
-    public double getCapacidadLibreTotal(List<CargaDeCamion> cargas) {
-        double libre = 0;
-        for (CargaDeCamion c : cargas) {
-            libre += c.getCapacidadDisponible();
-        }
-        return libre;
-    }
 }
 
 
